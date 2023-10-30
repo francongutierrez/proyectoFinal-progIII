31015093a1c1ba5a75579298b51dab26a0daad52
Franco <francogutierrez04@gmail.com>
2023-10-29 21:53:06 -0300

    Validacion en el back del formulario de busqueda

--- php/scripts/busqueda.php
+++ php/scripts/busqueda.php
@@ -1,49 +1,59 @@
 <?php
     
     if (isset($_POST['buscar'])) {
+
         extract($_POST);
 
-
-        include 'scripts/conexion_db.php';
-
-        // Consulta dinamica
-        $sql_busqueda = "SELECT * FROM propiedades WHERE 1";
-
-        if (!empty($titulo)) {
-            $titulo = strtolower($titulo);
-            $sql_busqueda .= " AND LOWER(titulo) LIKE '%$titulo%'";
+        if (empty($tiempoMaximo)) {
+            echo '<div class="container"><h6 class="mensajeError">Debe ingresar un tiempo máximo</h6></div>';
         }
-        if (!empty($ubicacion)) {
-            $ubicacion = strtolower($ubicacion);
-            $sql_busqueda .= " AND LOWER(ubicacion) LIKE '%$ubicacion%'";
-        }
-        if (!empty($costo)) {
-            $sql_busqueda .= " AND costo <= $costo";
-        }
-        if (!empty($tiempoMaximo)) {
-            $sql_busqueda .= " AND tiempo_maximo <= $tiempoMaximo";
-        }
-        if (!empty($cupo)) {
-            $sql_busqueda .= " AND cupo = $cupo";
-        }
-        if (!empty($inicioDisponibilidad) && !empty($finDisponibilidad)) {
-            $sql_busqueda .= " AND id NOT IN 
-            (SELECT id_propiedad FROM alquileres 
-            WHERE fecha_inicio BETWEEN '$inicioDisponibilidad' AND '$finDisponibilidad'
-            AND fecha_fin BETWEEN '$inicioDisponibilidad' AND '$finDisponibilidad'
-            AND (estado = 'pendiente' OR estado = 'activo'))";
-        }        
+        elseif (empty($inicioDisponibilidad) || empty($finDisponibilidad)) {
+            echo '<div class="container"><h6 class="mensajeError">Debe especificar las fechas de alquiler un tiempo máximo</h6></div>';
+        } else {
+            include 'scripts/conexion_db.php';
 
-        $sql_busqueda .= " AND activa = 1 AND id_dueno != $_SESSION[id_usuario]";
+            // Consulta dinamica
+            $sql_busqueda = "SELECT * FROM propiedades WHERE 1";
+    
+            if (!empty($titulo)) {
+                $titulo = strtolower($titulo);
+                $sql_busqueda .= " AND LOWER(titulo) LIKE '%$titulo%'";
+            }
+            if (!empty($ubicacion)) {
+                $ubicacion = strtolower($ubicacion);
+                $sql_busqueda .= " AND LOWER(ubicacion) LIKE '%$ubicacion%'";
+            }
+            if (!empty($costo)) {
+                $sql_busqueda .= " AND costo <= $costo";
+            }
+            if (!empty($tiempoMaximo)) {
+                $sql_busqueda .= " AND tiempo_maximo <= $tiempoMaximo";
+            }
+            if (!empty($cupo)) {
+                $sql_busqueda .= " AND cupo = $cupo";
+            }
+            if (!empty($inicioDisponibilidad) && !empty($finDisponibilidad)) {
+                $sql_busqueda .= " AND id NOT IN 
+                (SELECT id_propiedad FROM alquileres 
+                WHERE fecha_inicio BETWEEN '$inicioDisponibilidad' AND '$finDisponibilidad'
+                AND fecha_fin BETWEEN '$inicioDisponibilidad' AND '$finDisponibilidad'
+                AND (estado = 'pendiente' OR estado = 'activo'))";
+            }        
+    
+            $sql_busqueda .= " AND activa = 1 AND id_dueno != $_SESSION[id_usuario]";
+    
+            $_SESSION['criterios'] = $sql_busqueda;
+            
+            $_SESSION['fechaInicioDisponibilidad'] = $inicioDisponibilidad;
+            $_SESSION['fechaFinDisponibilidad'] = $finDisponibilidad;
+    
+            $conn->close();
+    
+            echo "<script>window.location.href = 'http://localhost/ProgramacionIII/proyectoFinal/php/resultados.php'</script>";
+        }
 
-        $_SESSION['criterios'] = $sql_busqueda;
-        
-        $_SESSION['fechaInicioDisponibilidad'] = $inicioDisponibilidad;
-        $_SESSION['fechaFinDisponibilidad'] = $finDisponibilidad;
 
-        $conn->close();
 
-        echo "<script>window.location.href = 'http://localhost/ProgramacionIII/proyectoFinal/php/resultados.php'</script>";
     }
 
 
